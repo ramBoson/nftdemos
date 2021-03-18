2e75b6522ddfb7b723a591c23915c3163777d8b1
ramBoson <ramamugesh95@gmail.com>
2021-03-18 19:05:33 +0530

    delete all

--- src/Newpage.js
+++ src/Newpage.js
@@ -1,21 +1,5 @@
 import React from "react";
-import history from "./utils/history";
-
-import {useState} from 'react';
-import web3 from './web3';
-//import lottery from './nftcontract';//this line import lottery folder
-//import ipfs from './ipfs';
-//import storehash from './storehash';
-
-import { Router, Route, Switch } from "react-router-dom";
-
-//import First from "./First";
-//import Second from "./Second";
-import Nft from "./Nft";
-import Sendpage from "./Sendpage";
-import Tokencreate from "./Tokencreate";
-import Printallimage from "./Printallimage";
-
+//import history from "./utils/history";
 
 
 
--- src/Nft.js
+++ src/Nft.js
@@ -1,17 +1,4 @@
 import './App.css';
-import {useState} from 'react';
-import web3 from './web3';
-import lottery from './nftcontract';//this line import lottery folder
-import ipfs from './ipfs';
-
-import { Router, Route, Switch } from "react-router-dom";
-import history from "./utils/history";
-
-import NewPage from './Newpage';
-import Tokencreate from './Tokencreate';
-import PrintallImage from './Printallimage';
-import Sendpage from './Sendpage';
-
 
 function App() {
 
