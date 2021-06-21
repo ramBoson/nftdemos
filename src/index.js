285f50a58728bbc4da00b70db954ecea892f52a6
ramBoson <ramamugesh95@gmail.com>
2021-06-22 00:39:28 +0530

    updated algo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+//import React from "react";
+//import ReactDOM from "react-dom";
+//import App from "./App";
+
+//ReactDOM.render(<App />, document.getElementById("root"));
+
+
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+//import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Routes from "./Routes";
+
+//ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
